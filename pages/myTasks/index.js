1ed26ba560d49aff63cfbb550e1fa33097009a79
Iftekher-CSE <108381759+Iftekher-CSE@users.noreply.github.com>
2022-12-29 20:19:21 +0600

    my tasks added

--- pages/myTasks/index.js
+++ pages/myTasks/index.js
@@ -1,15 +1,47 @@
+import { async } from "@firebase/util";
 import Head from "next/head";
-import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import TaskDetails from "../../components/TaskDetails";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const MyTasks = () => {
+    const { user } = useContext(AuthContext);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allTasks/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setTasks(data);
+            });
+    }, [user]);
+
     return (
         <div>
             <Head>
                 <title>My Task | Task Tracker</title>
             </Head>
             <h1>All My Tasks</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {tasks.map(task => (
+                    <TaskDetails key={task._id} task={task}></TaskDetails>
+                ))}
+            </div>
         </div>
     );
 };
 
 export default MyTasks;
+
+// export const getStaticProps = async AuthContext => {
+//     const { user } = useContext(AuthContext);
+//     const res = await fetch(`http://localhost:5000/allTasks/${user.email}`);
+//     const data = await res.json();
+//     console.log(data);
+//     return {
+//         props: {
+//             tasks: data,
+//         },
+//     };
+// };
