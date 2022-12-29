0f543cfe37499b4d3af1b0aa7c641ee8737dfae3
Iftekher-CSE <108381759+Iftekher-CSE@users.noreply.github.com>
2022-12-29 15:05:36 +0600

    footer changed

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
+import NavBar from "./NavBar";
 import Footer from "./Footer";
-import NavBar from "./navbar";
 
 const Layout = ({ children }) => {
     return (
         <>
             <NavBar></NavBar>
             <main>{children}</main>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </>
     );
 };
