c88ac3f904c656de188ef1a86eeb777a82845bee
Iftekher-CSE <108381759+Iftekher-CSE@users.noreply.github.com>
2022-12-30 17:10:09 +0600

    delete task added

--- components/TaskDetails.js
+++ components/TaskDetails.js
@@ -1,15 +1,28 @@
 import { Button, Card, Dropdown, Label, TextInput } from "flowbite-react";
+import { useRouter } from "next/router";
+import { toast } from "react-hot-toast";
 
 export default function TaskDetails({ task }) {
+    const router = useRouter();
+
+    const deleteHandler = () => {
+        fetch(`http://localhost:5000/allTasks/${task._id}`, {
+            method: "DELETE",
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if (data.deletedCount > 0) {
+                    toast.success("Deleted Confirmed");
+                    router.push("/addTask");
+                }
+            });
+    };
     return (
         <div className="max-w-sm">
             <Card>
                 <div className="flex flex-col items-center pb-10">
-                    <img
-                        className="mb-3 h-24 w-24 rounded-full shadow-lg"
-                        src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
-                        alt="Bonnie image"
-                    />
+                    <img className="mb-3 shadow-lg" src={task.taskImage} alt="task image" />
                     <h5 className="mb-1 text-xl font-medium text-red-900 dark:text-white">{task.taskName}</h5>
                     <div>
                         <div className="mb-2 block">
@@ -17,7 +30,7 @@ export default function TaskDetails({ task }) {
                         </div>
                         <div className="flex flex-col md:flex-row gap-2">
                             <TextInput id="taskDetails" type="text" required={true} defaultValue={task.taskDetails} />
-                            <Button type="submit">Submit</Button>
+                            <Button type="submit">Update Task</Button>
                         </div>
                     </div>
                     <div className="mt-4 flex space-x-3 lg:mt-6">
@@ -27,12 +40,12 @@ export default function TaskDetails({ task }) {
                         >
                             Task Completed
                         </a>
-                        <a
-                            href="#"
+                        <button
+                            onClick={deleteHandler}
                             className="inline-flex items-center rounded-lg border border-red-300 bg-white py-2 px-4 text-center text-sm font-medium text-red-900 hover:bg-red-100 focus:outline-none focus:ring-4 focus:ring-red-200 dark:border-red-600 dark:bg-red-800 dark:text-white dark:hover:border-red-700 dark:hover:bg-red-700 dark:focus:ring-red-700"
                         >
                             Delete Task
-                        </a>
+                        </button>
                     </div>
                 </div>
             </Card>
